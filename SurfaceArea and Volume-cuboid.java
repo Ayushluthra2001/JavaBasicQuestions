fe5b6b64c1b8f65fe044df3c368e8b74e8056198
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-12-26 11:34:39 +0530

    Create SurfaceArea and Volume-cuboid.java

---
+++ SurfaceArea and Volume-cuboid.java
@@ -0,0 +1,11 @@
+static int Surface_Area(int l, int b, int h){
+   // your code here
+      int ans= 2*l*h+2*b*l+2*h*b;
+      return ans;
+    }
+
+
+    static int Volume(int l, int b, int h){
+       // your code here
+       return l*b*h;
+    }
