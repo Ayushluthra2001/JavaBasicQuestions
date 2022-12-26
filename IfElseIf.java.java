5fb53e44ee8419e46346662b2234881686d0f60d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-12-26 16:30:56 +0530

    Create IfElseIf.java.java

---
+++ IfElseIf.java.java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if(n==1) System.out.println("one");
+        else if(n==2) System.out.println("two");
+        else if(n==3) System.out.println("three");
+        else if(n==4) System.out.println("four");
+        else if(n==5) System.out.println("five");
+        else System.out.println("Greater than 5");
+    }
+}
