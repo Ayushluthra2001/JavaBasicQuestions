1d06755534318830f4f630a21e912a4ee773b7e4
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-12-26 11:18:47 +0530

    Create Arithmetic Operators.java

---
+++ Arithmetic Operators.java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scan = new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
